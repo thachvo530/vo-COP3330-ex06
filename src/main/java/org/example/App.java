dec3aa4412ab89f00a07a0b4c191d89ff6e83e45
Thach Vo <thachvo530@yahoo.com>
2021-09-11 17:40:26 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,41 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Thach Vo
+ */
+
+package org.example;
+
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.print("What is your current age? ");
+        Scanner age = new Scanner(System.in);
+        String strage = age.next();
+
+        System.out.print("At what age would you like to retire? ");
+        Scanner retireage = new Scanner(System.in);
+        String strretireage = retireage.next();
+
+        int age1 = Integer.parseInt(strage);
+        int age2 = Integer.parseInt(strretireage);
+
+        int total = age2 - age1;
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        int totalyear = year + total;
+
+        System.out.print("You have "+total+" years left until you can retire.\n");
+        System.out.print("It's "+year+", so you can retire in "+totalyear+".");
+
+
+
+
+    }
+}
